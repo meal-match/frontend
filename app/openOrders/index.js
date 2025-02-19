49a9e4ace28f269db7731e5a683ec0759c958c30
Logan Millet <loganmillet38@gmail.com>
2025-02-19 09:31:16 -0600

    refactored orderlist/emptyorder components into scrollview

--- app/openOrders/index.js
+++ app/openOrders/index.js
@@ -59,144 +59,125 @@ const OpenOrders = () => {
     }
 
     const orderList = (
-        <ScrollView
-            refreshControl={
-                <RefreshControl
-                    refreshing={refreshing}
-                    onRefresh={onRefresh}
-                    tintColor={'black'}
-                    colors={['black']}
-                />
-            }
+        <List.AccordionGroup
+            expandedId={expandedId}
+            onAccordionPress={(id) => handleAccordianPress(id)}
         >
-            <List.AccordionGroup
-                expandedId={expandedId}
-                onAccordionPress={(id) => handleAccordianPress(id)}
+            <List.Accordion
+                title={'Buy Orders'}
+                id={1}
+                right={(props) =>
+                    expandedId === 1 ? (
+                        <Ionicons
+                            {...props}
+                            name="chevron-down-outline"
+                            size={28}
+                        />
+                    ) : (
+                        <Ionicons
+                            {...props}
+                            name="chevron-forward-outline"
+                            size={28}
+                        />
+                    )
+                }
+                left={(props) => (
+                    <Ionicons {...props} name="cart-outline" size={28} />
+                )}
             >
-                <List.Accordion
-                    title={'Buy Orders'}
-                    id={1}
-                    right={(props) =>
-                        expandedId === 1 ? (
-                            <Ionicons
-                                {...props}
-                                name="chevron-down-outline"
-                                size={28}
-                            />
-                        ) : (
+                {buyOrders.map((item) => (
+                    <List.Item
+                        key={item._id}
+                        title={item.meal.entree}
+                        onPress={() => handleItemPress(item)}
+                        right={(props) => (
                             <Ionicons
                                 {...props}
                                 name="chevron-forward-outline"
                                 size={28}
                             />
-                        )
-                    }
-                    left={(props) => (
-                        <Ionicons {...props} name="cart-outline" size={28} />
-                    )}
-                >
-                    {buyOrders.map((item) => (
-                        <List.Item
-                            key={item._id}
-                            title={item.meal.entree}
-                            onPress={() => handleItemPress(item)}
-                            right={(props) => (
-                                <Ionicons
-                                    {...props}
-                                    name="chevron-forward-outline"
-                                    size={28}
-                                />
-                            )}
-                            left={(props) => (
-                                <Image
-                                    {...props}
-                                    source={icons[item.restaurant]}
-                                />
-                            )}
-                            style={
-                                orders.indexOf(item) !== orders.length - 1
-                                    ? {
-                                          borderBottomColor: '#828A8F',
-                                          borderBottomWidth: 1
-                                      }
-                                    : {}
-                            }
+                        )}
+                        left={(props) => (
+                            <Image {...props} source={icons[item.restaurant]} />
+                        )}
+                        style={
+                            orders.indexOf(item) !== orders.length - 1
+                                ? {
+                                      borderBottomColor: '#828A8F',
+                                      borderBottomWidth: 1
+                                  }
+                                : {}
+                        }
+                    />
+                ))}
+            </List.Accordion>
+            <List.Accordion
+                title={'Sell Orders'}
+                id={2}
+                right={(props) =>
+                    expandedId === 2 ? (
+                        <Ionicons
+                            {...props}
+                            name="chevron-down-outline"
+                            size={28}
                         />
-                    ))}
-                </List.Accordion>
-                <List.Accordion
-                    title={'Sell Orders'}
-                    id={2}
-                    right={(props) =>
-                        expandedId === 2 ? (
-                            <Ionicons
-                                {...props}
-                                name="chevron-down-outline"
-                                size={28}
-                            />
-                        ) : (
+                    ) : (
+                        <Ionicons
+                            {...props}
+                            name="chevron-forward-outline"
+                            size={28}
+                        />
+                    )
+                }
+                left={(props) => (
+                    <Ionicons {...props} name="cash-outline" size={28} />
+                )}
+            >
+                {sellOrders.map((item) => (
+                    <List.Item
+                        key={item._id}
+                        title={item.meal.entree}
+                        onPress={() => handleItemPress(item)}
+                        right={(props) => (
                             <Ionicons
                                 {...props}
                                 name="chevron-forward-outline"
                                 size={28}
                             />
-                        )
-                    }
-                    left={(props) => (
-                        <Ionicons {...props} name="cash-outline" size={28} />
-                    )}
-                >
-                    {sellOrders.map((item) => (
-                        <List.Item
-                            key={item._id}
-                            title={item.meal.entree}
-                            onPress={() => handleItemPress(item)}
-                            right={(props) => (
-                                <Ionicons
-                                    {...props}
-                                    name="chevron-forward-outline"
-                                    size={28}
-                                />
-                            )}
-                            left={(props) => (
-                                <Image
-                                    {...props}
-                                    source={icons[item.restaurant]}
-                                />
-                            )}
-                            style={
-                                orders.indexOf(item) !== orders.length - 1
-                                    ? {
-                                          borderBottomColor: '#828A8F',
-                                          borderBottomWidth: 1
-                                      }
-                                    : {}
-                            }
-                        />
-                    ))}
-                </List.Accordion>
-            </List.AccordionGroup>
-        </ScrollView>
+                        )}
+                        left={(props) => (
+                            <Image {...props} source={icons[item.restaurant]} />
+                        )}
+                        style={
+                            orders.indexOf(item) !== orders.length - 1
+                                ? {
+                                      borderBottomColor: '#828A8F',
+                                      borderBottomWidth: 1
+                                  }
+                                : {}
+                        }
+                    />
+                ))}
+            </List.Accordion>
+        </List.AccordionGroup>
     )
 
-    const emptyOrders = (
-        <ScrollView
-            refreshControl={
-                <RefreshControl
-                    refreshing={refreshing}
-                    onRefresh={onRefresh}
-                    tintColor={'black'}
-                    colors={['black']}
-                />
-            }
-        >
-            <Text>You have no open orders</Text>
-        </ScrollView>
-    )
+    const emptyOrders = <Text>You have no open orders</Text>
 
     return (
         <Page header="Open Orders">
-            {orders && orders.length > 0 ? orderList : emptyOrders}
+            <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                        tintColor={'black'}
+                        colors={['black']}
+                    />
+                }
+            >
+                {orders && orders.length > 0 ? orderList : emptyOrders}
+            </ScrollView>
         </Page>
     )
 }
